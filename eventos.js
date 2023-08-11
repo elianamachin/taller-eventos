e6507264562cd10b4cccb8a1e3fd2cdac18349d3
USER_NAME <USER_EMAIL>
2023-08-11 16:46:41 -0300

    eventos.js

---
+++ eventos.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.getElementById("saludar");
+    const miBoton = document.getElementById("saludar");
+    
+    container.style.padding = "5px";
+    container.style.backgroundColor = "green";
+  });
+
+
+
+
